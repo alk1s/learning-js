71da848f73c0e75d65ba7f6f3444a3aa52fa0b36
Edward Pasenidis <me@edpasenidis.tech>
2020-05-26 01:14:42 +0300

    fix

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify, request
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy import func, select
 from database import Click, db
@@ -9,30 +8,45 @@ import traceback
 
 app = Flask(__name__)
 app.total_clicks = 0
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
-limiter = Limiter(
-    app,
-    key_func=get_remote_address,
-    default_limits=["200 per day", "50 per hour"]
-)
+CORS(app)
+limiter = Limiter(app, key_func=lambda: request.remote_addr,
+                  default_limits=["200 per day", "50 per hour"])
+
 
 @app.route('/api/counts', methods=['GET', 'POST'])
-@cross_origin()
 @limiter.limit("5/second", override_defaults=False)
 def main_endpoint():
     if request.method == 'GET':
         return jsonify({'clicks': app.total_clicks})
     elif request.method == 'POST':
         app.total_clicks += 1
+        db.add(Click(app.total_clicks, request.remote_addr))
+        db.commit()
+        return "", 204
+    else:
+        return "", 405
 
-        try:
-            db.add(Click(app.total_clicks, request.headers.get("HTTP_CF_CONNECTING_IP") or request.remote_addr))
-            db.commit()
-            return "", 204
-        except:
-            traceback.print_exc()
-            return jsonify({'message': traceback.format_exc()}), 500
+
+@app.before_request
+def before_request():
+    request.remote_addr = request.headers.get(
+        "CF-Connecting-IP") or request.headers.get("X-Forwarded-For") or request.remote_addr
+
+
+@app.errorhandler(Exception)
+def handle_500():
+    traceback.print_exc()
+    return jsonify({"message": traceback.format_exc()}), 500
+
+
+@app.errorhandler(404)
+def handle_404(error):
+    return jsonify({"message": "The requested route was not found"}), 404
+
+
+@app.errorhandler(405)
+def handle_405(error):
+    return jsonify({"message": "Method not allowed"}), 405
 
 
 @app.teardown_appcontext
